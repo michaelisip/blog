cc2726e344f43b54039a010cb2a483353a6f9032
MJ Isip <michael@webx.group>
2020-01-07 14:46:15 +0800

    setup jest

---
+++ src/components/__tests__/sample.js
@@ -0,0 +1,10 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import BlogIndex from "../../pages/index"
+
+describe("True", () => {
+  it("true", () => {
+    expect(true).toBeTruthy();
+  })
+})
