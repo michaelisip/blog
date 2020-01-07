aa1fef04b0d853332fbf4c9ed9c1a1a065ee96bf
MJ Isip <michael@webx.group>
2020-01-07 12:47:28 +0800

    edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Michael John Isip`,
+    author: `Michael John Isip`,
+    description: `Personal blogs.`,
+    siteUrl: `https://ehemji.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ehemji`,
     },
   },
   plugins: [
